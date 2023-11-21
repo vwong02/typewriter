3aaa5d2ddb823bf87e0bad86a8d46b88bb5859a3
Victoria Wong <victoria.ys.wong@gmail.com>
2023-11-21 10:11:56 -0500

    added file

---
+++ typewriter.js
@@ -0,0 +1,112 @@
+const sentence = "hello there from lighthouse labs";
+
+// for (const char of sentence) {
+  setTimeout(() => {
+    process.stdout.write('h');
+  }, 0);
+
+  setTimeout(() => {
+    process.stdout.write('e');
+  }, 50);
+
+  setTimeout(() => {
+    process.stdout.write('l');
+  }, 100);
+
+  setTimeout(() => {
+    process.stdout.write('l');
+  }, 150);
+
+  setTimeout(() => {
+    process.stdout.write('o');
+  }, 200);
+
+  setTimeout(() => {
+    process.stdout.write(' ');
+  }, 250);
+
+  setTimeout(() => {
+    process.stdout.write('f');
+  }, 300);
+
+  setTimeout(() => {
+    process.stdout.write('r');
+  }, 350);
+
+  setTimeout(() => {
+    process.stdout.write('o');
+  }, 400);
+
+  setTimeout(() => {
+    process.stdout.write('m');
+  }, 450);
+
+  setTimeout(() => {
+    process.stdout.write(' ');
+  }, 500);
+
+  setTimeout(() => {
+    process.stdout.write('l');
+  }, 550);
+
+  setTimeout(() => {
+    process.stdout.write('i');
+  }, 600);
+  
+  setTimeout(() => {
+    process.stdout.write('g');
+  }, 650);
+
+  setTimeout(() => {
+    process.stdout.write('h');
+  }, 700);
+
+  setTimeout(() => {
+    process.stdout.write('t');
+  }, 750);
+
+  setTimeout(() => {
+    process.stdout.write('h');
+  }, 800);
+
+  setTimeout(() => {
+    process.stdout.write('o');
+  }, 850);
+
+  setTimeout(() => {
+    process.stdout.write('u');
+  }, 900);
+
+  setTimeout(() => {
+    process.stdout.write('s');
+  }, 950);
+
+  setTimeout(() => {
+    process.stdout.write('e');
+  }, 1000);
+
+  setTimeout(() => {
+    process.stdout.write(' ');
+  }, 1050);
+
+  setTimeout(() => {
+    process.stdout.write('l');
+  }, 1100);
+
+  setTimeout(() => {
+    process.stdout.write('a');
+  }, 1150);
+
+  setTimeout(() => {
+    process.stdout.write('b');
+  }, 1200);
+
+  setTimeout(() => {
+    process.stdout.write('s');
+  }, 1250);
+
+  setTimeout(() => {
+    process.stdout.write('\n');
+  }, 1300);
+
+// }
